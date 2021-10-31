569d0926670424c0e1475817ea83c67a3b18ebe7
gcpromuga <gcpromuga@letran-calamba.edu.ph>
2021-10-31 21:01:26 +0800

    Add list view

--- app/src/main/java/com/example/login/ViewActivity.java
+++ app/src/main/java/com/example/login/ViewActivity.java
@@ -3,12 +3,28 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ViewActivity extends AppCompatActivity {
+
+    private ListView listRecord;
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand","India", "China", "australia", "Portugle", "America", "NewZealand"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+
+        initComponents();
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_view_record, R.id.textViewFullName, countryList);
+        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(this, R.layout.list_view_record, R.id.textViewAge, countryList);
+        listRecord.setAdapter(arrayAdapter1);
+        listRecord.setAdapter(arrayAdapter);
+    }
+
+    private void initComponents() {
+        listRecord = (ListView) findViewById(R.id.listViewRecord);
     }
 }
